2f1976885b389574435cf9d6097e2bf945d47a59
gbtsui <gabriel.tsui@protonmail.com>
2025-09-13 14:24:42 -0400

    feat!(frontend): migrated to nextjs

---
+++ floradex-frontend/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div className={""}>
+            <div className={"justify-self-center flex flex-col m-10"}>
+                <div className={"text-9xl p-5 mt-32 bg-foreground text-background rounded-xl rotate-2"}>
+                    flora<span className={"text-olivine"}>dex</span>
+                </div>
+                <div></div>
+            </div>
+        </div>
+    );
+}
