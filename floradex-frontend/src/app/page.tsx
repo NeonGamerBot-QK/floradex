613f2f39b3f490afb5b6d0267238e3f6ac7d4283
gbtsui <gabriel.tsui@protonmail.com>
2025-09-13 16:37:37 -0400

    minor fixes, tailwind classes still broken

--- floradex-frontend/src/app/page.tsx
+++ floradex-frontend/src/app/page.tsx
@@ -1,15 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={""}>
-      <div className={"justify-self-center flex flex-col m-10"}>
+      <div className={"justify-self-center flex flex-col m-10 gap-10"}>
         <div
           className={"text-9xl p-5 mt-32 text-background rounded-xl rotate-2"}
         >
-          <img src="/floradex.svg" />
+          <img src="/floradex.svg" alt={"floradex logo"} draggable={false} className={"animate-bounce"}/>
         </div>
-        <div></div>
+        <div className={"flex flex-row gap-4"}>
+            <Link
+                className={"p-5 rounded-xl text-xl bg-mindaro text-space-cadet"}
+                href={"/login"}>
+                Login
+            </Link>
+        </div>
       </div>
     </div>
   );
