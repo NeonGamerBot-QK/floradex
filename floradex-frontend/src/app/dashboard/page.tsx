8b09b4a416a63718c02bccf6667644c6cb545d9b
Selena Nguyen <snguyen3320@hwdsb.on.ca>
2025-09-13 20:44:01 -0400

    links to species and garden

--- floradex-frontend/src/app/dashboard/page.tsx
+++ floradex-frontend/src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function File() {
   return (
@@ -6,10 +7,20 @@ export default function File() {
       <div className="bg-space-cadet rounded-lg justify-self-center">
         <div className={"text-xl"}>
           <h1 className={"text-400"}>
-            hello hello hello hello hello hello hello hello
+            Your Dashboard
           </h1>
+          <div className={""}>
+            <Link className={""} href={"/catalog"}>  
+              Species
+            </Link>
+          </div>
+          <div className={""}>
+            <Link href={"/gambling"}>
+              The Garden
+            </Link>
+          </div>
         </div>
-      </div>
+        </div>
       <div className={"justify-self-end pr-12 w-200"}>
         <img className={" object-fit: fill rounded-lg"} src="/banner.JPG" />
       </div>
