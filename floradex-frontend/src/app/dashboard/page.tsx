c6c64932dfcec6bf86627b7c70ad6610f71be8c8
Selena Nguyen <snguyen3320@hwdsb.on.ca>
2025-09-13 21:27:58 -0400

    yay looking buttons

--- floradex-frontend/src/app/dashboard/page.tsx
+++ floradex-frontend/src/app/dashboard/page.tsx
@@ -5,21 +5,24 @@ export default function File() {
   return (
     <div className="grid grid-cols-2 gap-12 pt-10">
       <div className="bg-space-cadet rounded-lg justify-self-center">
-        <div className={"text-xl"}>
-          <h1 className={"text-400"}>
+          <h1 className={"text-xl"}>
             Your Dashboard
           </h1>
-          <div className={""}>
-            <Link className={""} href={"/catalog"}>  
-              Species
+          <div className={"grid grid-cols-2 gap-20"}>
+            <div className={"p-3 rounded-xl text-xl bg-mindaro text-space-cadet"}>
+              <Link 
+                href={"/catalog"}>  
+                  Species
             </Link>
+            </div>
+
+            <div className={"p-3 rounded-xl text-xl bg-mindaro text-space-cadet"}>
+              <Link 
+                href={"/gambling"}>
+                  The Garden
+              </Link>
+            </div>
           </div>
-          <div className={""}>
-            <Link href={"/gambling"}>
-              The Garden
-            </Link>
-          </div>
-        </div>
         </div>
       <div className={"justify-self-end pr-12 w-200"}>
         <img className={" object-fit: fill rounded-lg"} src="/banner.JPG" />
