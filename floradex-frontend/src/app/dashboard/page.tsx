5138cd08ca7dec58dbe1b33884d44db439bc3a89
Selena Nguyen <snguyen3320@hwdsb.on.ca>
2025-09-13 23:37:44 -0400

    yay

--- floradex-frontend/src/app/dashboard/page.tsx
+++ floradex-frontend/src/app/dashboard/page.tsx
@@ -3,24 +3,31 @@ import Link from "next/link";
 
 export default function File() {
   return (
-    <div className="grid grid-cols-2 gap-12 pt-10">
-      <div className="bg-space-cadet rounded-lg justify-self-center p-5">
+    <div>
+    <img 
+      className={"pt-4"}
+      src="/floradex.svg">
+
+    </img>
+    <div className="grid grid-cols-2 gap-12 pt-6">
+      <div className="bg-yinmin-blue rounded-lg justify-self-center p-5">
           <h1 className={"text-4xl"}>
             Your Dashboard
           </h1>
 
-          <image>
+          <img className={"w-40"} 
+            src="/window.svg">
+          </img>
 
-          </image>
-          <div className={"grid grid-cols-2 gap-50 pt-4"}>
-            <div className={"p-3 rounded-xl text-3xl bg-mindaro text-space-cadet"}>
+          <div className={"grid grid-cols-2 gap-46 pt-4"}>
+            <div className={"justify-self-center px-17 py-2 rounded-xl text-3xl bg-mindaro text-space-cadet"}>
               <Link 
                 href={"/catalog"}>  
                   Species
             </Link>
             </div>
 
-            <div className={"p-3 rounded-xl text-3xl bg-mindaro text-space-cadet"}>
+            <div className={"justify-self-center px-11 py-2 rounded-xl text-3xl bg-mindaro text-space-cadet"}>
               <Link 
                 href={"/gambling"}>
                   The Garden
@@ -28,9 +35,10 @@ export default function File() {
             </div>
           </div>
         </div>
-      <div className={"justify-self-end pr-12 w-200"}>
+      <div className={"justify-self-end pr-8 w-200"}>
         <img className={" object-fit: fill rounded-lg"} src="/banner.JPG" />
       </div>
+    </div>
     </div>
   );
 }
