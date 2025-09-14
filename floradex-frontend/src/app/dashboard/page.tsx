01292cccc9fa5656bb51d2fb637a091c904cffb6
Selena Nguyen <snguyen3320@hwdsb.on.ca>
2025-09-13 22:54:00 -0400

    bigger font hehe

--- floradex-frontend/src/app/dashboard/page.tsx
+++ floradex-frontend/src/app/dashboard/page.tsx
@@ -4,19 +4,23 @@ import Link from "next/link";
 export default function File() {
   return (
     <div className="grid grid-cols-2 gap-12 pt-10">
-      <div className="bg-space-cadet rounded-lg justify-self-center">
-          <h1 className={"text-xl"}>
+      <div className="bg-space-cadet rounded-lg justify-self-center p-5">
+          <h1 className={"text-4xl"}>
             Your Dashboard
           </h1>
-          <div className={"grid grid-cols-2 gap-20"}>
-            <div className={"p-3 rounded-xl text-xl bg-mindaro text-space-cadet"}>
+
+          <image>
+
+          </image>
+          <div className={"grid grid-cols-2 gap-50 pt-4"}>
+            <div className={"p-3 rounded-xl text-3xl bg-mindaro text-space-cadet"}>
               <Link 
                 href={"/catalog"}>  
                   Species
             </Link>
             </div>
 
-            <div className={"p-3 rounded-xl text-xl bg-mindaro text-space-cadet"}>
+            <div className={"p-3 rounded-xl text-3xl bg-mindaro text-space-cadet"}>
               <Link 
                 href={"/gambling"}>
                   The Garden
